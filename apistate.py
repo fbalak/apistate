d567c046c2e2d7304e8cc6e7b1670ab38639f040
Filip Balák <fbalak@redhat.com>
2017-02-21 12:28:42 +0100

    add apistate script

---
+++ apistate.py
@@ -0,0 +1,90 @@
+import os
+import re
+import glob
+import requests
+
+sources = [
+        {"files": glob.glob('../api/docs/*'),
+            "pattern": 'api/1.0/',
+            "type": "file",
+            "column": "documented"},
+        {"files": glob.glob('../api/*'),
+            "pattern": 'get \'/',
+            "type": "file",
+            "column": "implemented"},
+        {"files": glob.glob('../usmqe-tests/usmqe/api/tendrlapi/*'),
+            "pattern": '(P|p)attern: *"',
+            "type": "file",
+            "column": "covered"},
+        {"url": "http://hostname/api/1.0/Flows",
+            "pattern": 'flows',
+            "type": "web",
+            "column": "flows"}
+        ]
+
+aliases = [
+        {"pattern": ".{8}-.{4}-.{4}-.{4}-.{12}", "repl": ":integration_id"},
+        {"pattern": ":cluster_id:", "repl": ":integration_id"},
+        {"pattern": ":job_id", "repl": ":integration_id"},
+        ]
+
+
+table = {}
+
+for source in sources:
+    if source["column"] not in table:
+        table[source["column"]] = []
+
+    if source["type"] == "file":
+        for file_path in source['files']:
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                    for i in range(0, len(lines)):
+                        try:
+                            line = "{}{}{}".format(
+                                lines[i],
+                                lines[i+1],
+                                lines[i+2])
+                            line = line.replace("\n", "")
+                            for alias in aliases:
+                                line = re.sub(
+                                    alias["pattern"],
+                                    alias["repl"],
+                                    line)
+                            # print(line)
+                            p = re.search(source['pattern'], line)
+                            url = line[p.span()[1]:].split(' ')[0]\
+                                .strip("-").strip("\",").strip("'")
+
+                            # print("{}".format(url))
+                            if url not in table[source["column"]]:
+                                table[source["column"]].append(url)
+                        except Exception as err:
+                            # print("{}".format(err))
+                            pass
+
+    elif source["type"] == "web":
+        data = requests.get(source["url"]).json()
+        for i in data[source["pattern"]]:
+            table[source["column"]].append(i["name"])
+
+
+used = {}
+rows = []
+
+
+rows = {}
+max_i = max([len(table[x]) for x in table.keys()])
+for i in range(0, max_i):
+    for key in table.keys():
+        try:
+            if table[key][i] not in rows:
+                rows[table[key][i]] = dict.fromkeys(table.keys(), "")
+            rows[table[key][i]][key] = table[key][i]
+        except:
+            pass
+
+print(",".join(table.keys()))
+for i in sorted(rows):
+    print(",".join(rows[i].values()))
